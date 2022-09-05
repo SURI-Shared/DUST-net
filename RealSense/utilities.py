bde1ed8b17c0583beebf906158fefea4166de3c4
Geordan Gutow <ggutow@andrew.cmu.edu>
2022-09-05 16:36:38 -0400

    code to plot axes as arrow on an image

--- RealSense/utilities.py
+++ RealSense/utilities.py
@@ -14,10 +14,21 @@ import time
 import torch
 from matplotlib import pyplot
 
-from cv2 import createBackgroundSubtractorMOG2
+import cv2 as cv
 
 from model.model import VMStiefelSVDNet
 from utils.utils import convert_predictions_VMStSVD
+
+def estimation_to_camera_transformation():
+    #frame used for estimation is x pointing out, y pointing right, z pointing down
+    #but cv.projectPoints wants a camera frame with x pointing right, y pointing down, z pointing out
+    #R is thus the matrix that maps points in the estimation frame to the opencv camera frame
+    #note that the two frames have the same origin so 0 translation component is needed
+    t=np.zeros((3,1))
+    R=np.array([[0.0, 1, 0],
+       [0, 0, 1],
+       [1, 0, 0]])
+    return R,t
 
 def record_frames(dur):
     pipeline=rs.pipeline()
@@ -49,6 +60,30 @@ def get_depth_scale(profile=None):
         profile=config.resolve(wrapper)
     return profile.get_device().first_depth_sensor().get_depth_scale()
 
+def get_depth_intrinsics(profile=None):
+    if profile is None:
+        pipeline=rs.pipeline()
+        config=rs.config()
+        wrapper=rs.pipeline_wrapper(pipeline)
+        profile=config.resolve(wrapper)
+    streams=profile.get_streams()
+    for stream in streams:
+        if stream.stream_type()==rs.stream.depth:
+            return stream.as_video_stream_profile().get_intrinsics()
+    print("No depth stream found")
+
+def get_color_intrinsics(profile=None):
+    if profile is None:
+        pipeline=rs.pipeline()
+        config=rs.config()
+        wrapper=rs.pipeline_wrapper(pipeline)
+        profile=config.resolve(wrapper)
+    streams=profile.get_streams()
+    for stream in streams:
+        if stream.stream_type()==rs.stream.color:
+            return stream.as_video_stream_profile().get_intrinsics()
+    print("No color stream found")    
+
 def remove_background(cimages,dimages,detectShadows=False):
     '''
     DUST-net expects everything except the two bodies of interest to have 0 depth
@@ -58,7 +93,7 @@ def remove_background(cimages,dimages,detectShadows=False):
     @param detectShadows: should the opencv background subtractor do shadow detection (default False)
     @return masked cimages, masked dimages using foreground mask obtained from cimages via opencv BackgroundSubtractorMOG2
     '''
-    backSub=createBackgroundSubtractorMOG2(detectShadows=detectShadows)
+    backSub=cv.createBackgroundSubtractorMOG2(detectShadows=detectShadows)
     masks=np.array([backSub.apply(ci) for ci in cimages])//255
     return (masks*cimages.transpose((3,0,1,2))).transpose((1,2,3,0)),masks*dimages
         
@@ -115,7 +150,127 @@ def plot_axis_estimates(prediction,ax3d,clear=False):
     ax3d.set_ylabel("Y")
     ax3d.set_zlabel("Z")
 
+def axis_in_depth_image_coord(label,intrinsics):
+    l=np.array(label[...,:3])
+    m=np.array(label[...,3:6])
+    p=np.cross(l,m)
+    ends=l+p
+    return to_image_coordinates(p, intrinsics),to_image_coordinates(ends, intrinsics)
 
+def to_image_coordinates(points,intrinsics):
+    R,t=estimation_to_camera_transformation()
+    #camera intrinsics are stored using the pyrealsense2 class, but we just need the camera intrinsics matrix:
+    intrinsic_matr=np.array([[intrinsics.fx,0,intrinsics.ppx],[0,intrinsics.fy,intrinsics.ppy],[0,0,1]])
+    #and the distortion coefficients, which the pyrealsense2 sets to 0 for the D435i depth and color streams
+    distortion_coeffs=np.array(intrinsics.coeffs)
+    rot_rodrigues=cv.Rodrigues(R)[0]
+    transformed=cv.projectPoints(points,rot_rodrigues,t,intrinsic_matr,distortion_coeffs)[0]
+    return tuple(transformed.squeeze().astype(np.int64))
+
+def add_axis_to_image(image,label,intrinsics,width=2):
+    tail,head=axis_in_depth_image_coord(label, intrinsics)
+    return add_arrow_to_image(image, tail, head, width)
+
+def add_arrow_to_image(image,tail,head,width):
+    if len(image.shape)>2:
+        #color image
+        linecolor=(255,255,0)
+    else:
+        #depth image
+        linecolor=(255,)
+    lined=cv.arrowedLine(image.copy(),tail,head,linecolor,width,line_type=cv.FILLED)
+    return lined
+
+def slide_axis_into_view(label,intrinsics):
+    '''
+    find a pair of points on the axis that are within the camera's field of view
+    '''
+    l=np.array(label[:3])
+    m=np.array(label[3:6])
+    p=np.cross(l,m)
+    
+    R,t=estimation_to_camera_transformation()
+    fx=intrinsics.fx
+    fy=intrinsics.fy
+    cx=intrinsics.ppx
+    cy=intrinsics.ppy
+    width=intrinsics.width
+    height=intrinsics.height
+    #u,v are pixel horizontal, vertical locations respectively
+    #we seek constraints on lambda such that lambda*l+p is in the FOV
+    upper_bounds=[]
+    lower_bounds=[]
+    #u must be positive
+    uposcoeff=fx*R[0]+cx*R[2]
+    uposbound=-np.dot(uposcoeff,p)/np.dot(uposcoeff,l)
+    if np.dot(uposcoeff,l)<0:#if negative, lambda must be LESS than the bound.
+        upper_bounds.append(uposbound)
+    else:
+        lower_bounds.append(uposbound)
+    
+    #u must be less than the width of the FOV in pixels
+    uwidcoeff=fx*R[0]+(cx-width)*R[2]
+    uwidbound=-np.dot(uwidcoeff,p)/np.dot(uwidcoeff,l)
+    if np.dot(uwidcoeff,l)>=0:#if negative, lambda must be GREATER than the bound.
+        upper_bounds.append(uwidbound)
+    else:
+        lower_bounds.append(uwidbound)
+    #v must be positive
+    vposcoeff=fy*R[1]+cy*R[2]
+    vposbound=-np.dot(vposcoeff,p)/np.dot(vposcoeff,l)
+    if np.dot(vposcoeff,l)<0:#if negative, lambda must be LESS than the bound.
+        upper_bounds.append(vposbound)
+    else:
+        lower_bounds.append(vposbound)    
+    #u must be less than the width of the FOV in pixels
+    vheicoeff=fy*R[1]+(cy-height)*R[2]
+    vheibound=-np.dot(vheicoeff,p)/np.dot(vheicoeff,l)
+    if np.dot(vheicoeff,l)>=0:#if negative, lambda must be GREATER than the bound.
+        upper_bounds.append(vheibound)
+    else:
+        lower_bounds.append(vheibound)    
+    if len(lower_bounds)==0:
+        greatest_lower=-np.inf
+    else:
+        greatest_lower=np.max(lower_bounds)
+    if len(upper_bounds)==0:
+        least_upper=np.inf
+    else:
+        least_upper=np.min(upper_bounds)
+    if least_upper<greatest_lower:
+        raise ValueError("To be in FOV lambda must be both <{u} and >{l}".format(u=least_upper,l=greatest_lower))
+    else:
+        if 0>greatest_lower and 0<least_upper:
+            #if possible use the location the network picked
+            l1=0
+            pslid=p
+        else:
+            #if not, translate as little as possible
+            #note that in this case, both bounds have the same sign!
+            l1=min((abs(greatest_lower),abs(least_upper)))*np.sign(greatest_lower)
+            pslid=p+l1*l
+        #then, scale l such that pslid+lscaled is still in the FOV
+        #3,0,1:l2>-1,l2<2
+        #first, try to not scale at all:
+        if l1+1<least_upper and l1+1>greatest_lower:
+                lscaled=l
+        else:
+            #would run off the edge in at least one direction
+            positive_scaling=least_upper-l1
+            negative_scaling=greatest_lower-l1
+            if np.abs(positive_scaling)>np.abs(negative_scaling):
+                scale=min((abs(positive_scaling),1))*np.sign(positive_scaling)
+            else:
+                scale=min((abs(negative_scaling),1))*np.sign(negative_scaling)
+        if scale<0:
+            #swap ends of arrow to keep the sign the same
+            pslid=pslid+scale*l
+            scale*=-1
+        lscaled=scale*l
+        return to_image_coordinates(pslid, intrinsics),to_image_coordinates(pslid+lscaled, intrinsics)
+    
+    
+    
 class IndexTracker:
     '''
     Call with an axis and a stack of images
