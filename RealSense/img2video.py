63c5a59b3e355b574e9b845e27cac68902cda8d9
Geordan Gutow <ggutow@andrew.cmu.edu>
2022-09-15 10:02:01 -0400

    code to convert a bunch of image files to a video

---
+++ RealSense/img2video.py
@@ -0,0 +1,24 @@
+'''
+Created on Sep 8, 2022
+
+@author: ggutow
+'''
+import skvideo.io as skio
+import cv2 as cv
+import numpy as np
+from os import path
+import glob
+import sys
+if __name__=="__main__":
+    args=sys.argv
+    folder=args[1]
+    img_extension=args[2]
+    output_name=args[3]
+    imgfiles=sorted(glob.glob(path.expanduser(path.join(folder,"*."+img_extension))))
+    output=path.join(folder,output_name)
+    print(output)
+    video=skio.FFmpegWriter(output,outputdict={'-r':'10'})#tuple(reversed(img.shape[:2])))
+    for imgfile in imgfiles:
+        img=cv.imread(imgfile)
+        video.writeFrame(img[:,:,::-1])
+    video.close()
